b02d90c2b6847c5d46030843c8c3882ffcae6dfe
Gopalbhople <gbhople63@gmail.com>
2026-03-18 02:31:22 +0530

    solve cors error

--- backend/first.js
+++ backend/first.js
@@ -435,6 +435,8 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174", "https://todo1-qtld-jotddhd13-gopal-arun-bhoples-projects.vercel.app/"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 const PORT = process.env.PORT || 3232;
